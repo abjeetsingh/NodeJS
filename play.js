7d8fd260f66233de5bcfd1514da473164b049150
Abjeet Singh <abjeets50@gmail.com>
2023-01-21 13:36:03 +0530

    revising

---
+++ play.js
@@ -0,0 +1,15 @@
+const person = {
+  name: "Abjeet",
+  age: 20,
+  sayHello() {
+    console.log("Hello I Am" + this.name);
+  }
+};
+// created a dictionary
+
+// creating a list
+
+let val = ["Hello", "Abjeet"];
+let coppiedval = [...val]; // spread operator
+let fun = (...args) =>  args.map((v) => v+4);
+console.log(fun(1,2,3,4,5,6,7));
