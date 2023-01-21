d39597df0318a3506c20c12d4664ad60013ba431
Abjeet Singh <abjeets50@gmail.com>
2023-01-21 23:14:46 +0530

    started with node

---
+++ app.js
@@ -0,0 +1,11 @@
+/* 
+A few Core Modules that we sould know about are
+
+Http - Hyper Text Transfer Protocol --> launch serves or send request
+Https - Hyper Text Transfer Protocol Secure
+fs- File System
+os - operating system
+path - Constructing path to file systems as mac , lenux and windows use differnet paths
+
+
+*/ 
